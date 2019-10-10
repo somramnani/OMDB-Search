e8a396da540dbd34557ac0611d5d2460c76efb41
Som Ramnani <som.ramnani@gmail.com>
2019-10-10 11:18:54 -0500

    addded on click shows the API

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 // ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 const express = require("express");
 // ==============================================================================
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 // ==============================================================================
-
-// Tells node that we are creating an "express" server
 var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -23,8 +17,6 @@ app.use(express.json());
 
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 require("./app/routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
@@ -32,7 +24,6 @@ require("./app/routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
-// The below code effectively "starts" our server
 // =============================================================================
 app.listen(PORT, () => {
   console.log("listening on port 3000");
