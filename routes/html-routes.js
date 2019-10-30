018ae7edb7a656cf477a9db6729f3a261b853ae7
Som Ramnani <som.ramnani@gmail.com>
2019-10-30 16:03:55 -0500

    in html-routes deleted ' if the user already has an account send them to the members page'

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,25 +5,25 @@ module.exports = function(app) {
   //HTML ROUTES
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
   //
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
   //
